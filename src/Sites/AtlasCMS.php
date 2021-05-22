44ddbd6d753c2972ca33950724b5410759dddbe6
Toby Twigger <tobytwigger1@gmail.com>
2021-05-22 02:46:01 +0100

    Add the Atlas Site

---
+++ src/Sites/AtlasCMS.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Atlas\Sites;
+
+use OriginEngine\Helpers\Env\EnvRepository;
+use OriginEngine\Helpers\WorkingDirectory\WorkingDirectory;
+use OriginEngine\Pipeline\Pipeline;
+use OriginEngine\Site\Sail;
+use OriginEngine\Site\Site;
+use OriginEngine\Site\SiteBlueprint;
+
+class AtlasCMS extends SiteBlueprint
+{
+
+    public function name(): string
+    {
+        // TODO: Implement name() method.
+    }
+
+    public function getUrl(Site $site): string
+    {
+        $envRepository = new EnvRepository($site->getWorkingDirectory());
+        $env = $envRepository->get('.env.local');
+
+        $url = $env->getVariable('APP_URL');
+        $port = $env->getVariable('APP_PORT');
+
+        return sprintf('%s:%s', $url, $port);
+    }
+
+    public function getStatus(Site $site): string
+    {
+        if(!app(\OriginEngine\Contracts\Instance\InstanceRepository::class)->exists($site->getInstanceId())) {
+            return Site::STATUS_MISSING;
+        }
+
+        if(Sail::isRunning($site)) {
+            return Site::STATUS_READY;
+        }
+
+        return Site::STATUS_DOWN;
+    }
+
+    public function getInstallationPipeline(): Pipeline
+    {
+        // TODO: Implement getInstallationPipeline() method.
+    }
+
+    public function getUninstallationPipeline(): Pipeline
+    {
+        // TODO: Implement getUninstallationPipeline() method.
+    }
+
+    public function getSiteUpPipeline(): Pipeline
+    {
+        // TODO: Implement getSiteUpPipeline() method.
+    }
+
+    public function getSiteDownPipeline(): Pipeline
+    {
+        // TODO: Implement getSiteDownPipeline() method.
+    }
+}
